1494b038e9cd04dfe6224ad9b9143ac122ccc350
Ashwanth <ashwanth1109@gmail.com>
2019-12-15 11:14:12 +0530

    Added color palette

---
+++ src/components/colors.js
@@ -0,0 +1,8 @@
+const deepSaffron = "#F9983E";
+const mauvelous = "#F39BA6";
+
+const cobaltBlue = "#034CA5";
+const hanBlue = "#2B6DCD";
+const blueBerry = "#5085E3";
+
+export const bg = mauvelous;

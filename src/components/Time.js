7af7d7794d4cc1f5eaef5f3c0a88182fdd655c70
Ashwanth <ashwanth1109@gmail.com>
2019-12-15 13:01:13 +0530

    Display two digits in timer

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Time } from "./styles";
 
 export default ({ value }) => {
-  return <Time>{value}</Time>;
+  const displayValue = useCallback(val => {
+    if (val < 10) return `0${val}`;
+    return val;
+  }, []);
+  return <Time>{displayValue(value)}</Time>;
 };

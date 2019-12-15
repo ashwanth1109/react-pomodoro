bded76e344f856ec19c56cd5da5cdd6effad79dd
Ashwanth <ashwanth1109@gmail.com>
2019-12-15 17:45:08 +0530

    Set lower bound for counter

--- src/components/App.js
+++ src/components/App.js
@@ -13,26 +13,29 @@ import useInterval from "./useInterval";
 export default () => {
   const [isRunning, setIsRunning] = useState(false);
   const [time, setTime] = useState({
-    seconds: 0,
-    minutes: 25
+    minutes: 25,
+    seconds: 0
   });
 
   const decrementTime = useCallback(() => {
     setTime(prev => {
       if (prev.seconds === 0) {
         return {
-          seconds: 59,
-          minutes: prev.minutes - 1
+          minutes: prev.minutes - 1,
+          seconds: 59
         };
       } else
         return {
-          seconds: prev.seconds - 1,
-          minutes: prev.minutes
+          minutes: prev.minutes,
+          seconds: prev.seconds - 1
         };
     });
   }, [time]);
 
-  useInterval(decrementTime, isRunning ? 1000 : null);
+  useInterval(
+    decrementTime,
+    isRunning && (time.minutes !== 0 || time.seconds !== 0) ? 1000 : null
+  );
 
   const { minutes, seconds } = time;
 
